3faefa4c10c046460ca02c98049548a0c37101ce
Erick_Martinez <erickmartinezhuaylla123@gmail.com>
2019-04-25 09:06:54 -0400

    Actualización

--- app/src/main/java/bo/Androides/MiCafe/MainActivity.java
+++ app/src/main/java/bo/Androides/MiCafe/MainActivity.java
@@ -1,15 +1,30 @@
 package bo.Androides.MiCafe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.content.Context;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+
+    public void registrarClick(View view) {
+        Toast.makeText(MainActivity.this,"EL CLICK FUNCIONA", Toast.LENGTH_SHORT).show();
+
     }
 }
