4167f6bf0ef9dd7e69c002f84e8501dbfc20bda1
ManuMoll <43225686+ManuMoll@users.noreply.github.com>
2019-05-12 20:25:09 -0400

    bases de datos anadida para platos

--- app/src/main/java/bo/Androides/MiCafe/DetallesPlatos.java
+++ app/src/main/java/bo/Androides/MiCafe/DetallesPlatos.java
@@ -1,8 +1,11 @@
 package bo.Androides.MiCafe;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,24 +13,34 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import bo.Androides.MiCafe.db.DatabaseHelper;
+import bo.Androides.MiCafe.model.Pedido;
+
 public class DetallesPlatos extends AppCompatActivity {
 
     private Spinner opciones;
     private Context mContext;
+    private DatabaseHelper dbHelper;
+    private TextView nombreProductos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_platos);
-        mContext = this;
-
+        this.mContext = this;
+        dbHelper = new DatabaseHelper(this.mContext);
+        nombreProductos = findViewById(R.id.titulodescripcionPlatos);
         opciones = (Spinner) findViewById(R.id.elegirPlatos);
     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.opciones, android.R.layout.simple_spinner_item);
         opciones.setAdapter(adapter);
 
     TextView titulo = (TextView) findViewById(R.id.titulodescripcionPlatos);
     TextView detalles = (TextView) findViewById(R.id.descripcionPlatos);
+    TextView precio = (TextView) findViewById(R.id.precioPlatos);
 
     Intent intent = getIntent();
     Bundle b = ((Intent) intent).getExtras();
@@ -35,10 +48,10 @@ public class DetallesPlatos extends AppCompatActivity {
         if (b != null) {
             titulo.setText(b.getString("TIT"));
             detalles.setText(b.getString("DET"));
+            precio.setText(b.getString("Precio"));
+
         }
     }
-
-
 
 
     public void verPedidoPlatos (View view){
@@ -48,6 +61,57 @@ public class DetallesPlatos extends AppCompatActivity {
 
     public void anadidoPlatos (View view){
         Toast.makeText(this, "Añadido a su lista de pedidos", Toast.LENGTH_SHORT).show();
+
+        Pedido carrito = new Pedido();
+        carrito.setProducto(nombreProductos.getText().toString());
+        //    carrito.setProducto(precioProducto.getText().toString());
+        /*
+        carrito.setPrecio(precio);
+        carrito.setCantidad(cantidad);
+        */
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        dbHelper.agregarAlCarrito(carrito);
+
+        String json = new Gson().toJson(carrito);
+        Log.e("Pedido enviado", json);
+
+        //    llenarPedido(nombreProducto.getText().toString(),precioProducto.getText().toString());
+        llenarPedido(nombreProductos.getText().toString());
+
+        Intent intent = new Intent();
+        intent.putExtra(Constants.KEY_REGISTRAR_PEDIDO, json);
+        setResult(RESULT_OK, intent);
+        finish();
     }
+
+    private void llenarPedido (String nombrePedido){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(Constants.PREF_PEDIDO, nombrePedido);
+        // editor.putString(Constants.PREF_PRECIO, precioBebida);
+        editor.apply();
+    }
+    public void verpedidoPlatos(View view){
+
+        String nombrePedido = nombreProductos.getText().toString();
+        Log.e("Mis datos", nombrePedido );
+
+
+        if (validarPedido(nombrePedido)){
+            Intent intent3 = new Intent(this, VerPedido.class);
+            //    intent3.putExtra(Constants.KEY_NOMBRE_PEDIDO, nombrePedido);
+            startActivity(intent3);
+            //   startActivityForResult(intent3, Constants.CODIGO_TRANSACCION_PRODUCTO);
+        } else {
+            Toast.makeText(mContext, "Pedido incorrecto", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private boolean validarPedido(String nombrePedido){
+        if (nombrePedido == null ){
+            return false;
+        }
+        return true;
+    }
+
 
 }
--- app/src/main/java/bo/Androides/MiCafe/PlatosALaCarta.java
+++ app/src/main/java/bo/Androides/MiCafe/PlatosALaCarta.java
@@ -1,10 +1,14 @@
 package bo.Androides.MiCafe;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,51 +34,73 @@ public class PlatosALaCarta extends AppCompatActivity {
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent visorDetalles = new Intent(view.getContext(), DetallesPlatos.class);
+                Intent visorDetallesPlatos = new Intent(view.getContext(), DetallesPlatos.class);
                 Producto producto = productos.get(position);
-                visorDetalles.putExtra("TIT", producto.getNombre());
-                visorDetalles.putExtra("DET", producto.getDetalle());
-                startActivity(visorDetalles);
+                visorDetallesPlatos.putExtra("TIT", producto.getNombre());
+                visorDetallesPlatos.putExtra("DET", producto.getDetalle());
+                visorDetallesPlatos.putExtra("DET", producto.getPrecio());
+                // startActivity(visorDetallesPlatos);
+                startActivityForResult(visorDetallesPlatos, Constants.CODIGO_TRANSACCION_PRODUCTO);
             }
         });
-    }
-
-    private void llenarProductos() {
-        productos.add(new Producto(1,
-                "Alitas",
-                "Plato con deliciosas alitas y papas",
-                20,
-                R.drawable.alitas));
-        productos.add(new Producto(2,
-                "Asado a la olla",
-                "Estofado de carne acompañado con arroz y papas cocidas",
-                18,
-                R.drawable.asado_a_la_olla));
-        productos.add(new Producto(3,
-                "Hamburguesa",
-                "Hamburguesa de carne de res acompañada con queso y ensalada; aparte vienen papas fritas",
-                15,
-                R.drawable.hamburguesa));
-        productos.add(new Producto(4,
-                "Lasagna",
-                "Hojas de pasta italiana con salsa bechamel y bolognesa",
-                20,
-                R.drawable.lasagna));
-        productos.add(new Producto(5,
-                "Nuggets",
-                "Nuggets acompañados de papas y salsa golf",
-                20,
-                R.drawable.nuggets));
-        productos.add(new Producto(6,
-                "Papas a la huancaina",
-                "Papas servida con la autentica salsa peruana a la huancaina",
-                18,
-                R.drawable.papas_a_la_huancaina));
-        productos.add(new Producto(7,
-                "Pollo a la plancha",
-                "Pollo cocido en sarten con un minimo de aceite servido con papas y arroz",
-                18,
-                R.drawable.pollo_a_la_plancha));
 
     }
-}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Constants.CODIGO_TRANSACCION_PRODUCTO) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    String json = data.getStringExtra(Constants.KEY_REGISTRAR_PEDIDO);
+                    Log.e("Pedido Recibido", json);
+
+                    Producto producto = new Gson().fromJson(json, Producto.class);
+                    //   nombreProducto.setText(producto.getNombre());
+
+                }
+            }
+        }
+    }
+
+    private void llenarProductos () {
+                productos.add(new Producto(1,
+                        "Alitas",
+                        "Plato con deliciosas alitas y papas",
+                        20,
+                        R.drawable.alitas));
+                productos.add(new Producto(2,
+                        "Asado a la olla",
+                        "Estofado de carne acompañado con arroz y papas cocidas",
+                        18,
+                        R.drawable.asado_a_la_olla));
+                productos.add(new Producto(3,
+                        "Hamburguesa",
+                        "Hamburguesa de carne de res acompañada con queso y ensalada; aparte vienen papas fritas",
+                        15,
+                        R.drawable.hamburguesa));
+                productos.add(new Producto(4,
+                        "Lasagna",
+                        "Hojas de pasta italiana con salsa bechamel y bolognesa",
+                        20,
+                        R.drawable.lasagna));
+                productos.add(new Producto(5,
+                        "Nuggets",
+                        "Nuggets acompañados de papas y salsa golf",
+                        20,
+                        R.drawable.nuggets));
+                productos.add(new Producto(6,
+                        "Papas a la huancaina",
+                        "Papas servida con la autentica salsa peruana a la huancaina",
+                        18,
+                        R.drawable.papas_a_la_huancaina));
+                productos.add(new Producto(7,
+                        "Pollo a la plancha",
+                        "Pollo cocido en sarten con un minimo de aceite servido con papas y arroz",
+                        18,
+                        R.drawable.pollo_a_la_plancha));
+
+            }
+
+        }
+
