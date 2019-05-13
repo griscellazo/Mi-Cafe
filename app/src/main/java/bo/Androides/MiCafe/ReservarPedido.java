c8f7b6c07cedb34228cd1070d1458ab943583445
David Cáceres Callaú <49012187+DavidHumberto@users.noreply.github.com>
2019-05-12 21:56:49 -0400

    2do commit Secuencia de Layouts Completo y Titulos Corregidos

---
+++ app/src/main/java/bo/Androides/MiCafe/ReservarPedido.java
@@ -0,0 +1,18 @@
+package bo.Androides.MiCafe;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+
+public class ReservarPedido extends AppCompatActivity {
+
+    @RequiresApi (api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservarpedido);
+    }
+
+
+}
